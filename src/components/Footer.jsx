7d0781c9f61a71ebe9e1a3b6b6b11e1666f52279
Hoang <freeman.dev10@gmail.com>
2023-09-16 13:27:55 +0700

    update table

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
         <div className="bg-green min-h-[100vh] w-full pt-[100px] relative bottom-[0rem] overflow-hidden z-0">
           <div className="max-w-full">
             <img
-              className="none overflow-clip"
+              className="none overflow-clip w-full"
               src="https://www.emojibsc.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FFooterMobile.dbc43a69.png&w=828&q=75"
             />
           </div>
--- src/components/until/GetEmoji.jsx
+++ src/components/until/GetEmoji.jsx
@@ -1,6 +1,6 @@
 function GetEmoji() {
   return (
-    <div className="max-w-full bg-hero-3 mx-auto h-full block px-[100px] sm:px-[0.9375rem] py-[16.875rem] sm:py-[6.125rem] bg-cover bg-no-repeat bg-[length:100%_100%] scroll-smooth  after-content=[''] after:absolute after:w-full after:h-[25rem] after:bg-green after:left-0 after:right-0 after:-z-10">
+    <div className="max-w-full bg-hero-3 mx-auto h-full block px-[100px] sm:px-[0.9375rem] md:px-[3.375rem] py-[16.875rem] sm:py-[6.125rem] bg-cover bg-no-repeat bg-[length:100%_100%] scroll-smooth  after-content=[''] after:absolute after:w-full after:h-[25rem] after:bg-green after:left-0 after:right-0 after:-z-10">
       <div className="flex justify-center items-center min-w-full relative">
         <div className="w-[500px] sm:w-[300px]">
           <img src="https://www.emojibsc.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FGetEmojiTitle.834ee90d.png&w=1920&q=75" />
@@ -12,10 +12,10 @@ function GetEmoji() {
         </div>
       </div>
 
-      <div className="flex justify-center items-stretch my-[40px] sm:my-[0] gap-3 sm:flex-col max-w-full relative">
-        <div className="p-[40px] sm:p-[10px] flex-[0_0_50%] sm:flex-[0_0_100%]  sm:max-w-full">
+      <div className="flex justify-center items-stretch my-[40px] sm:my-[0] gap-3 sm:flex-col md:flex-col max-w-full relative">
+        <div className="p-[40px] sm:p-[10px] flex-[0_0_50%] sm:flex-[0_0_100%] md:flex-[0_0_100%] sm:max-w-full">
           <div className="bg-white rounded-lg overflow-hidden w-full drop-shadow-3xl">
-            <div className="bg-hero-4 bg-center-center bg-no-repeat bg-cover bg-[length:100%_100%] w-full">
+            <div className="bg-hero-4 bg-top-center bg-no-repeat bg-cover bg-[length:100%_100%] w-full">
               <div className="h-[300px] flex mt-[20px] px-[30px] items-center flex-col">
                 <h1 className="text-3xl font-wonderland">CONTRACT</h1>
                 <div className="w-full text-sm font-wonderland text-red text-center break-words">
